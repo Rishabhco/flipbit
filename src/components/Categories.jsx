58a23305a5c97d61bf90a50ba8d20cc2d38b63c5
Rishabhco <rishabhagrawal2020@gmail.com>
2023-08-18 19:13:55 +0530

    product component ready

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -5,7 +5,7 @@ const products = [
   {
     id: 1,
     name: 'Earthen Bottle',
-    href: '#',
+    href: 'earthernbottle',
     price: '$48',
     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
     imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
@@ -13,7 +13,7 @@ const products = [
   {
     id: 2,
     name: 'Nomad Tumbler',
-    href: '#',
+    href: 'tumbler',
     price: '$35',
     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
     imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
@@ -21,7 +21,7 @@ const products = [
   {
     id: 3,
     name: 'Focus Paper Refill',
-    href: '#',
+    href: 'refill',
     price: '$89',
     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
     imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
@@ -29,7 +29,7 @@ const products = [
   {
     id: 4,
     name: 'Machined Mechanical Pencil',
-    href: '#',
+    href: 'prencil',
     price: '$35',
     imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
@@ -76,7 +76,7 @@ export default function Categories() {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <Link key={product.id} to={product.href} className="group">
+            <Link key={product.id} to={`/categories/${id}/${product.href}`} className="group">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <img
                   src={product.imageSrc}
--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -3,6 +3,7 @@ import {Routes,Route} from 'react-router-dom';
 import Home from './Home';
 import Header from './Header';
 import Categories from "./Categories"
+import Product from "./Product"
 
 export default function Pages() {
   return (
@@ -11,6 +12,7 @@ export default function Pages() {
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/categories/:id" element={<Categories/>}/>
+            <Route path="/categories/:id/:productID" element={<Product/>}/>
             <Route path="/about" element={<div>About</div>}/>
             <Route path="/contact" element={<div>Contact</div>}/>
         </Routes>
